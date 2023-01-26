38ae599f897b6016c3a716a7c139ba4627cfe9b5
raashidbaba <raashidbaba849@gmail.com>
2023-01-26 23:04:58 +0530

    my first program.

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+       // System.out.println("hello world");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("please enter some input");
+        int rollNumb = scanner.nextInt();
+        System.out.println("this is my roll no" + rollNumb);
+    }
+}
